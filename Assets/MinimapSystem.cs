48a7a71f352e5ae656def3bc28b5d5722288d1de
pointcache <antonov.3d@gmail.com>
2017-07-28 17:58:44 -0500

    init

---
+++ Assets/MinimapSystem.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapSystem : MonoBehaviour {
+
+    private static MinimapSystem m_instance;
+    public static MinimapSystem Instance
+    {
+        get {
+            if (!m_instance)
+                m_instance = GameObject.FindObjectOfType<MinimapSystem>();
+            return m_instance;
+        }
+    }
+
+    private List<MinimapObject> m_minimapObjectsPool = new List<MinimapObject>();
+
+    
+    private class MinimapIcon {
+
+        public RectTransform iconTransform;
+        public Transform targetTransform;
+
+        public MinimapIcon(MinimapObject.Config config) {
+
+        }
+
+    }
+
+
+    internal void RegisterMMObject(MinimapObject obj) {
+        m_minimapObjectsPool.Add(obj);
+    }
+
+    internal void UnRegisterMMObject(MinimapObject obj) {
+        m_minimapObjectsPool.Remove(obj);
+    }
+
+    private void Update() {
+
+        int count = m_minimapObjectsPool.Count;
+
+        for (int i = 0; i < count; i++) {
+
+
+
+        }
+
+    }
+
+}
---
+++ Assets/Walker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walker : MonoBehaviour {
+
+    [SerializeField]
+    private float speed = 2f;
+
+     [SerializeField]
+    private float rotationspeed = 100f;
+
+    private void Update() {
+
+        transform.Translate(new Vector3(0, 0, Input.GetAxis("Vertical") * Time.deltaTime * speed), Space.Self);
+        transform.Rotate(new Vector3(0, 1, 0), Input.GetAxis("Horizontal")* Time.deltaTime * rotationspeed);
+
+    }
+
+
+}
